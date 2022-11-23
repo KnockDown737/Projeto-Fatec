b0f786aeae1cee47508f38020fbb3a95c2213603
André Martins <andre.martins.luiz@hotmail.com>
2022-11-23 15:05:51 -0300

    implementei o logo pro dark mode

--- js/main.js
+++ js/main.js
@@ -1,5 +1,17 @@
 
-      function darkMode() {
-        var element = document.body;
-        element.classList.toggle("darkmode");
+
+var imgAtual ="imgs/logo.svg"   /*logo normal*/
+var imgAnterior="imgs/logoTeste.png" /*logo em branco*/
+
+
+function darkMode() {
+  var element = document.body;
+  element.classList.toggle("darkmode");  
+  /*troca de cores do logo*/
+  document.getElementById("logo").src= imgAtual; /*Ele acessa o atributo src do id=logo e troca de imagem*/
+  let aux = imgAtual;
+  imgAtual = imgAnterior;  /* A imagem atual vira a imagem anterior */
+  imgAnterior = aux;
+
+
       }
