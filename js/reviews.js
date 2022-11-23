a0474e9d01c681e0a849512045042a2f20f1acda
Luan Costa <luan.coast@outlook.com.br>
2022-11-22 22:17:34 -0300

    Removei o código antigo(defeituoso) e adicionei cometarios de passo a passo

--- js/reviews.js
+++ js/reviews.js
@@ -6,7 +6,7 @@ const ul = document.querySelector('#review-content')
 form.onsubmit = function(event){
   event.preventDefault()
 
-//Const's criadas para pegar o valor dos inputs do formulário e depois t
+//Const's criadas para pegaros inputs do formulário e depois transferir o valor para um const para se utilizado
   const titulo = form.querySelector('#tGame')
   const genero = form.querySelector('#gGame')
   const descri = form.querySelector('#dGame')
@@ -17,13 +17,16 @@ form.onsubmit = function(event){
   const value3 = descri.value
   const value4 = nota.value
 
+//Se os values(1,2,3,4) estiverem vazios (sem valor) a função ira return (assim só poderá ser "ativada" se todos os inputs do form estiverem preenchidos)
   if(value1 == "" || value2 == "" || value3 == "" || value4 == "" ) return
 
-
+//Crio uma tag li dentro do ul(lista)
   const newLi = document.createElement('li')
+
   // ul.appendChild(newLi) Dessa forma o newli aparecia como Last-children
   ul.insertBefore(newLi, ul.children[0])  //Dessa forma o newli apareci como first-children
 
+//Insiro o modelo de HTML com os value's posicionados em suas respectivas tag's no li recém-criado
   newLi.innerHTML = `
   <div id="h2-span">
   <h2>${value1}</h2>
@@ -35,6 +38,7 @@ form.onsubmit = function(event){
   
   `
 
+//Limpo o valor(value) dos input's (assim deixo o formulário vazio de novo)
   titulo.value = ""
   genero.value = ""
   descri.value = ""
@@ -42,71 +46,17 @@ form.onsubmit = function(event){
 
 }
 
+
+//Botão usado para remover o item(li)
+//Função ativa pelo event click (envento de clicar no no item destinado)
 ul.onclick = function(event){
+  //Se o "alvo" do event click conter a class especifica(delete nesse caso)
   if(event.target.classList.contains('delete')){
+    //Irá aparecer um "alert/pop-up" de confirmação, se Sim segue, se Não cancela
     if(confirm("Irá deletar esse item. Tem certeza?")){
+      //Removerá o elemento "pai"(parentElement) do "alvo" do evento. Nesse caso como o alvo é uma tag button dentro da tag li, o li é considerado seu pai e então removido
       event.target.parentElement.remove()
     }
   }
 }
 
-
-// const form = document.querySelector('#addReview')
-// const ul = document.querySelector('#review-content')
-
-
-// form.onsubmit = function(event){
-//   event.preventDefault()
-
-
-  
-  
-
-//   const titulo = form.querySelector('#tGame')
-//   const genero = form.querySelector('#gGame')
-//   const descri = form.querySelector('#dGame')
-//   const nota = form.querySelector('#nGame')
-
-//   const value1 = titulo.value
-//   const value2 = genero.value
-//   const value3 = descri.value
-//   const value4 = nota.value
-
-//   if(value1 == "" || value2 == "" || value3 == "" || value4 == "" ) return
-
-  
-
-//   // li.querySelector('h2').textContent = value1
-//   // li.querySelector('.sp1').textContent = value2
-//   // li.querySelector('p').textContent = value3
-//   // li.querySelector('.sp2').textContent = value4
-
-//   const li = ul.innerHTML += `<li>
-//   <div id="h2-span">
-//   <h2>${value1}</h2>
-//   <span class="sp1">${value2}</span>
-//   </div>
-//   <span class="sp2">${value4}</span>
-//   <button class="delete" id="del-btn">X</button>
-//   <p>${value3}</p>
-//   </li>
-//   `
-
-//   titulo.value = ""
-//   genero.value = ""
-//   descri.value = ""
-//   nota.value = ""
-
-//   document.parentNode.appendChild(li)
-
-  
-
-// }
-
-// ul.onclick = function(event){
-//   if(event.target.classList.contains('delete')){
-//     if(confirm("Irá deletar esse item. Tem certeza?")){
-//       event.target.parentElement.remove()
-//     }
-//   }
-// }
