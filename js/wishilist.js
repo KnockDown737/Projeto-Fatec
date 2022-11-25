d9bd6b17cbbaeba27e71511b5cc7344509d72264
Germano <53625118+Homertex@users.noreply.github.com>
2022-11-25 14:14:54 +0000

    Primeira update no css do wishlist

--- js/wishilist.js
+++ js/wishilist.js
@@ -1,19 +1,19 @@
 function CreateRow() {
-  const tabela = document.querySelector("table");
+  const tabela = document.querySelector("#corpoTabela");
 
   const titulo = document.querySelector("#titulo");
   const preco = document.querySelector("#preco");
   const lancamento = document.querySelector("#lancamento");
 
-  const row = tabela.insertRow(2);
+  const row = tabela.insertRow(0);
   const cell1 = row.insertCell(0);
   const cell2 = row.insertCell(1);
   const cell3 = row.insertCell(2);
   const cell4 = row.insertCell(3);
   cell1.innerHTML = `<input type="text" class="titulo" readonly>`;
   cell2.innerHTML = `<input type="number" class="preco" readonly>`;
-  cell3.innerHTML = `<input type="text" class="lancamento" readonly>`;
-  cell4.innerHTML = `<button class="editar" onclick="editarLinha()">editar</button> <button class="excluir">excluir</button>`;
+  cell3.innerHTML = `<input type="date" class="lancamento" readonly>`;
+  cell4.innerHTML = `<button class="editar" onclick="editarLinha()">Editar</button> <button class="excluir">Excluir</button>`;
 
   cell1.querySelector('input').value = document.querySelector('#titulo').value
   cell2.querySelector('input').value = document.querySelector('#preco').value
@@ -39,9 +39,7 @@ function CreateRow() {
   const excluirGame = document.querySelector(".excluir")
 
   excluirGame.addEventListener('click', () => {
-    if(confirm("Deseja excluir esse item da Wishlist?")){
       row.remove();
-    }
   })
 
   titulo.value = "";
